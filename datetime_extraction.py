46a5e7e2dd77505bc81d739fcdf14041ff874f42
Sophie Mori <ksmori@mit.edu>
2016-09-17 22:25:36 -0400

    initial commit

---
+++ datetime_extraction.py
@@ -0,0 +1,30 @@
+import email
+import mailbox
+import os
+from dateutil import parser
+import datetime
+from datetime import timezone
+
+os.chdir('/home/ksmori/Desktop')
+ 
+mbox = mailbox.mbox('free_food.mbox')
+
+dates = [message.get("Date") for message in mbox]
+
+def boom(dt):
+	return dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
+
+freq = [[0 for col in range(24)] for row in range(7)]
+
+for date in dates:
+	try:
+		parsed_date = boom(parser.parse(date))
+		freq[parsed_date.weekday()][parsed_date.hour] += 1
+	except:
+		continue
+
+print(*range(24), sep='\t')
+print('\n')
+
+for row in freq:
+	print(*row, sep='\t')
